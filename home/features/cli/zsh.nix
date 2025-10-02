c7b7eb59764ea7e38a1f648a766ba2fc238d8174
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2025-10-01 23:21:35 -0400

    Zsh declaritive
    Made the .zshrc completely declaritive to avoid conflicting issues with having the one sourced from our .dotfiles. This should probabl be corrected

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -1,6 +1,6 @@
 # ZSH feature
 
-{ config, lib, inputs, osConfig, ... }:
+{ config, lib, inputs, osConfig, pkgs, ... }:
 with lib;
 let cfg = config.features.cli.zsh;
 in {
@@ -10,27 +10,37 @@ in {
   # If ZSH is enabled, apply these settings
   config = mkIf cfg.enable {
 
+    ### Removed: made zsh completely declarative (this diverges from the .dotfiles) ###
     # Adds our dotfiles
-    home.file.".zshrc.local" = { source = "${inputs.dotfiles}/.zshrc"; };
+    # home.file.".zshrc.local" = { source = "${inputs.dotfiles}/.zshrc"; };
     # home.file.".p10k.zsh" = { source = "${inputs.dotfiles}/.p10k.zsh"; };
 
-    # Creates a nix managed .zshrc, which sources our dotfiles
+    # Creates a nix managed .zshrc rather than our dotfile
     programs.zsh = {
       enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
 
       shellAliases = {
         grep = "rg";
         cat = "bat";
+        htop = "htop -t";
+        zoxideHist = "zoxide query -l -s | less";
+        reload-zsh = "source ~/.zshrc";
+        edit-zsh = "nvim ~/.zshrc";
         rebuild =
           "sudo nixos-rebuild switch --flake /home/ant/nix#${osConfig.networking.hostName}";
       };
 
-      initContent = ''
-        # Source the custom dotfiles
-        if [ -f ~/.zshrc.local ]; then
-          source ~/.zshrc.local
-        fi
-      '';
+      initContent = ''eval "$(zoxide init --cmd cd zsh)" # replaces cd'';
+
+      # initContent = ''
+      #   # Source the custom dotfiles
+      #   if [ -f ~/.zshrc.local ]; then
+      #     source ~/.zshrc.local
+      #   fi
+      # '';
     };
 
   };
